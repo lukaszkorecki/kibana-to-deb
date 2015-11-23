f06826d0f3da3f0770a54929ecb4ef8bed25943b
Łukasz Korecki <lukasz@korecki.me>
2015-11-23 10:32:03 +0000

    log & ignore

--- debify.sh
+++ debify.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
+
+log() {
+  logger -s -t DEBIFY -- "$*"
+}
+
 set -e
+
+# cleanup
+rm -rf ./tmp/*
+mkdir -p ./tmp
+rm -f *.tar.gz *.deb
 
 ROOT=$(pwd)
 KIBANA_VERSION=${1:-4.2.1}
 
+log "Downloading kibana ${KIBANA_VERSION}"
+
 KIBANA_NAME=kibana-${KIBANA_VERSION}-linux-x64
 
 KIBANA_URL=https://download.elastic.co/kibana/kibana/${KIBANA_NAME}.tar.gz
-
-
-mkdir -p ./tmp
-
-rm *.tar.gz
 
 
 curl -L ${KIBANA_VERSION} -O
@@ -20,9 +27,12 @@ mv  *.tar.gz tmp/
 
 cd tmp/
 
+log "Unpacking"
+
 tar xvfz ${KIBANA_NAME}.tar.gz
 cd ${KIBANA_NAME}
 
+log "Preparing the package"
 echo "web: ./bin/kibana" > Procfile
 
 pkgr package . \
@@ -34,4 +44,8 @@ pkgr package . \
   --compile-cache-dir=$cacheRoot/compile \
   --buildpacks-cache-dir=$cacheRoot/build
 
+log "Packaging done!"
+
 mv *.deb ${ROOT}/
+
+ls *.deb
